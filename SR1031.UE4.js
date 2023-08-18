60b38c6773cc1df3fee7eb48abb7026e5f11cbbd
VVZharski <106373839+VVZharski@users.noreply.github.com>
2023-08-18 11:33:54 +0300

    Update SR1031.UE4.js

--- SR1031.UE4.js
+++ SR1031.UE4.js
@@ -1167,9 +1167,9 @@ $(document).ready(function() {
 			});
 */
 // Instead as a fallback, download as ArrayBuffer. (TODO: Figure out the bugs with the above, and switch to using that one instead)
-			fetchOrDownloadAndStore(db, Module.locateFile('SR1031.data'), 'arraybuffer').then(function(dataArrayBuffer) {
+			fetchOrDownloadAndStore(db, Module.locateFile('https://dl.dropboxusercontent.com/scl/fi/csp5r7i67tubrve5bpjyy/SR1031.data?rlkey=mlv3mvpamouk6vjrg2amp22yr&dl=0'), 'arraybuffer').then(function(dataArrayBuffer) {
 				Module['preloadedPackages'] = {};
-				Module['preloadedPackages'][Module.locateFile('SR1031.data')] = dataArrayBuffer;
+				Module['preloadedPackages'][Module.locateFile('https://dl.dropboxusercontent.com/scl/fi/csp5r7i67tubrve5bpjyy/SR1031.data?rlkey=mlv3mvpamouk6vjrg2amp22yr&dl=0')] = dataArrayBuffer;
 				return dataJsDownload.then(addScriptToDom);
 			});
 
