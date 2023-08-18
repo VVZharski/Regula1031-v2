cd4ac472f8b1b64a96b3161345867594c0e49228
VVZharski <106373839+VVZharski@users.noreply.github.com>
2023-08-18 11:37:26 +0300

    Update SR1031.data.js

--- SR1031.data.js
+++ SR1031.data.js
@@ -18,8 +18,8 @@ Module.expectedDataFileDownloads++;
     } else {
       throw 'using preloaded data can only be done on a web page or in a web worker';
     }
-    var PACKAGE_NAME = 'D:/Unreal Projects/UE 4.23.1/SR1031/Binaries/HTML5/SR1031.data';
-    var REMOTE_PACKAGE_BASE = 'SR1031.data';
+    var PACKAGE_NAME = 'https://dl.dropboxusercontent.com/scl/fi/csp5r7i67tubrve5bpjyy/SR1031.data?rlkey=mlv3mvpamouk6vjrg2amp22yr&dl=0';
+    var REMOTE_PACKAGE_BASE = 'https://dl.dropboxusercontent.com/scl/fi/csp5r7i67tubrve5bpjyy/SR1031.data?rlkey=mlv3mvpamouk6vjrg2amp22yr&dl=0';
     if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
       Module['locateFile'] = Module['locateFilePackage'];
       err('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
